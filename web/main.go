05dab190416bb5647d5aba2cbbb34dd5be107728
mstuder95376 <mike@hamsoftware.org>
2019-04-25 20:47:06 -0700

    Added web server

---
+++ web/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>About</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	fmt.Println("Server starting ...")
+	fmt.Println("http://localhost:3000")
+	fmt.Println("http://localhost:3000/about")
+	http.ListenAndServe(":3000", nil)
+}
